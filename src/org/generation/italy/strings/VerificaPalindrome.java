05b70aa8c8756f0be02a6f47cd8fb44c9670be79
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-22 15:32:34 +0100

    primo commit

--- src/org/generation/italy/strings/VerificaPalindrome.java
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -1,10 +1,36 @@
 package org.generation.italy.strings;
 
+import java.util.Scanner;
+
 public class VerificaPalindrome {
 
 	public static void main(String[] args) {
-		
 
+		//chiedere all’utente di inserire una parola
+		//verificare se è palindroma
+		//stampare in console la lunghezza della parola e se è palindroma oppure no
+
+		String parola, parolaPalindroma = "";		
+		System.out.print("Inserisci la parola: ");
+		Scanner scan = new Scanner(System.in);
+		parola = scan.nextLine();
+		
+		int length = parola.length();				
+		
+		
+		for(int i = length -1; i >= 0 ; i--) {
+			 parolaPalindroma = parolaPalindroma + parola.charAt(i);
+			 
+			 if(parola.equals(parolaPalindroma)) {
+				 System.out.println("la parola " + parola + " è una parola palindroma " + "ed è lunga: " + length);
+				 break;
+				 
+			 }else {
+				 System.out.println("non lo è");
+			 }
+			 
+		}
+		scan.close();
 	}
 
 }
